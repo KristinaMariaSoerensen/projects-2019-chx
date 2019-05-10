3dd84e642f7949871bb91df9732df41491a49ef2
Astrid <astri.fj@gmail.com>
2019-05-10 17:07:05 +0900

    Update

--- modelproject/modelproject/chxmodelproject.py
+++ modelproject/modelproject/chxmodelproject.py
@@ -3,35 +3,42 @@ from scipy import optimize
 #%matplotlib inline
 import matplotlib.pyplot as plt
 
-def inflation(u, u_n, beta):
-    """ the basic Phillips-curve. Inflation as a function of unemployment.
-
-    Args:
-    u (float): Unemployment
-    u_n (float): Natural employment rate
-    beta (float): Parameter
-
-
-    Returns:
-        result (float): Inflation
-
-    """ 
-    return -beta * (u - u_n)
+def keynesian_cross(T, I, G, NX, a, b):
+    """ Draws the Keynesian cross with the 45-degree line and 
+    the planned total spending as a function of total production.
     
-
-def simple_phillips_curve(u_n, beta):
-    """ Illustrates the simple Phillips curve. The trade-off between low unemployment and low inflation
     Args:
-    u (float): Unemployment
-    u_n (float): Natural employment rate
-    beta (float): Parameter
+    AD (float): Aggregate expenditure
+    Y (float): Total production
+    T (float): Taxs
+    a (float): Constant consumption, a>0
+    b (float): Marginal consumption rate, 0<b<1
+    I (float): Investment
+    G (float): Public expenditure
+    NX (float): Net export
 
     Return: Figure
     """
 
-    u_arrey = np.linspace(0, 100)
-    pi_arrey = - beta * (u_arrey - u_n)
+    Y_arrey = np.linspace(0,300)
+    AD_arrey = (a + b * (Y_arrey - T) + I + G + NX)
+    degree = Y_arrey
+    return plt.plot(Y_arrey, degree), plt.plot(Y_arrey, AD_arrey)
 
-    return plt.plot(u_arrey, pi_arrey)
+def ss_cross(T, I, G, NX, a, b):
+    """ Steady state for the Keynesian cross where aggregate expenditure equals total production
     
-#def inflation_updated(u, u_n, beta)
+    Args:
+    AD (float): Aggregate expenditure
+    Y (float): Total production
+    T (float): Tax
+    a (float): Constant consumption, a>0
+    b (float): Marginal consumption rate, 0<b<1
+    I (float): Investment
+    G (float): Public expenditure
+    NX (float): Net export
+
+    Returns: 
+        Result: Steady state production, Y (float)
+    """
+    return 1/(1-b) * (I + G + NX + a - b*T)
