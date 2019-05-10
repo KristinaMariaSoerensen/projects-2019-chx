71561fd60a95b72dc1e4caa090078160ebcbc796
Astrid <astri.fj@gmail.com>
2019-05-10 21:38:19 +0900

    Update

--- modelproject/README.md
+++ modelproject/README.md
@@ -1,3 +1,9 @@
-# Modelproject
+## Model analysis project
 
-Should contain a short introduction to your project, and show how to produce your results.
+# The Keynesian Cross
+
+This project analyses a small economy and how the production in the short run is affected by shifts in net export. The model is analyzed with exogenous and endogenous taxes respectively. We see how this affects the economy's ability to withstand shifts in exogenous variables.
+
+The **results** of the project can be seen from running [modelproject.ipynb](modelproject.ipynb).
+
+**Dependencies:** Apart from a standard Anaconda Python 3 installation the project requires no further installations.
--- modelproject/modelproject/chxmodelproject.py
+++ modelproject/modelproject/chxmodelproject.py
@@ -23,10 +23,22 @@ def keynesian_cross(T, I, G, NX, a, b):
     Y_arrey = np.linspace(0,300)
     AD_arrey = (a + b * (Y_arrey - T) + I + G + NX)
     degree = Y_arrey
-    return plt.plot(Y_arrey, degree), plt.plot(Y_arrey, AD_arrey)
 
-def ss_cross(T, I, G, NX, a, b):
-    """ Steady state for the Keynesian cross where aggregate expenditure equals total production
+    fig = plt.figure(figsize=(8,6), dpi=100)
+    ax = fig.add_subplot(1,1,1)
+
+    ax.plot(degree, label="45-degree line")
+    ax.plot(AD_arrey, label="AD=C+I+G+NX")
+
+    ax.set_xlabel("Y")
+    ax.set_ylabel("AD")
+
+    ax.legend(loc="upper left")
+
+    return fig
+
+def cross_equalibrium(T, I, G, NX, a, b):
+    """ The equalibrium for the Keynesian cross where aggregate expenditure equals total production
     
     Args:
     AD (float): Aggregate expenditure
@@ -39,6 +51,82 @@ def ss_cross(T, I, G, NX, a, b):
     NX (float): Net export
 
     Returns: 
-        Result: Steady state production, Y (float)
+        Result: Production in equalibrium, Y (float)
     """
     return 1/(1-b) * (I + G + NX + a - b*T)
+
+def keynesian_cross_NXshift(T, I, G, NX, a, b, delta_NX):
+    """ Steady state for the Keynesian cross where aggregate expenditure equals total production
+    
+    Args:
+    AD (float): Aggregate expenditure
+    Y (float): Total production
+    T (float): Tax
+    a (float): Constant consumption, a>0
+    b (float): Marginal consumption rate, 0<b<1
+    I (float): Investment
+    G (float): Public expenditure
+    NX (float): Net export
+    delta_NX (float): Net export shift in 
+
+    Returns: 
+        Result: Figure
+    """
+    NX2 = NX + delta_NX
+    Y_arrey = np.linspace(0,300)
+    AD_arrey = (a + b * (Y_arrey - T) + I + G + NX)
+    AD2_arrey = (a + b * (Y_arrey - T) + I + G + NX2)
+    degree = Y_arrey
+
+    fig = plt.figure(figsize=(8,6), dpi=100)
+    ax = fig.add_subplot(1,1,1)
+
+    ax.plot(degree, label="45-degree line")
+    ax.plot(AD_arrey, label="AD=C+I+G+NX")
+    ax.plot(AD2_arrey, label="AD'=C+I+G+NX'")
+
+    ax.set_xlabel("Y")
+    ax.set_ylabel("AD")
+
+    ax.legend(loc="upper left")
+    
+    return fig
+
+def keynesian_cross_NXshift_t(k, t, I, G, NX, a, b, delta_NX):
+    """ Steady state for the Keynesian cross where aggregate expenditure equals total production
+    
+    Args:
+    AD (float): Aggregate expenditure
+    Y (float): Total production
+    k (float): Base tax
+    t (float): Marginal tax rate
+    a (float): Constant consumption, a>0
+    b (float): Marginal consumption rate, 0<b<1
+    I (float): Investment
+    G (float): Public expenditure
+    NX (float): Net export
+    delta_NX (float): Net export shift in 
+
+    Returns: 
+        Result: Figure
+    """
+    NX2 = NX + delta_NX
+    Y_arrey = np.linspace(0,300)
+    AD_arrey = (a + b * (Y_arrey - (k + b*t)) + I + G + NX)
+    AD2_arrey = (a + b * (Y_arrey - (k + b*t)) + I + G + NX2)
+    degree = Y_arrey
+
+    fig = plt.figure(figsize=(8,6), dpi=100)
+    ax = fig.add_subplot(1,1,1)
+
+    ax.plot(degree, label="45-degree line")
+    ax.plot(AD_arrey, label="AD=C+I+G+NX")
+    ax.plot(AD2_arrey, label="AD'=C+I+G+NX'")
+
+
+    ax.set_xlabel("Y")
+    ax.set_ylabel("AD")
+
+    ax.legend(loc="upper left")
+    
+    return fig
